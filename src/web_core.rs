44b1c25174497526868afac37a8f6368094a058e
MichaelFabrizio <mikefabrizio94@gmail.com>
2024-09-01 14:53:10 +0000

    Working PartialEq implementation for indices

--- src/web_core.rs
+++ src/web_core.rs
@@ -1,28 +1,78 @@
 use core::fmt::Debug;
+
 use std::mem::{align_of, size_of};
 use std::ptr::addr_of_mut;
 
 use super::{console_log, log};
 use crate::wasm_allocator::WasmAllocator;
 
-trait IndexVariable: Copy + Debug {
+trait UnsignedType: Copy + Debug {
     const MAX_VALUE: usize;
 }
 
-impl IndexVariable for u8 {
+impl UnsignedType for u8 {
     const MAX_VALUE: usize = 255;
 }
-impl IndexVariable for u16 {
+
+impl UnsignedType for u16 {
     const MAX_VALUE: usize = 65535;
 }
-impl IndexVariable for u32 {
+
+impl UnsignedType for u32 {
     const MAX_VALUE: usize = 4294967295;
 }
 
-pub(crate) struct KeyVector<T: Sized, I: IndexVariable, const N: usize> {
+trait IndexType: PartialEq<i32> {}
+
+struct Index<T: UnsignedType>(T);
+
+impl IndexType for Index<u8> {}
+impl IndexType for Index<u16> {}
+impl IndexType for Index<u32> {}
+
+impl PartialEq<i32> for Index<u8> {
+    fn eq(&self, other: &i32) -> bool {
+        // Safe to 'upcast' a u8 to i32 because no loss of bit information
+        if (self.0 as i32) == *other {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+impl PartialEq<i32> for Index<u16> {
+    fn eq(&self, other: &i32) -> bool {
+        // Safe to 'upcast' a u16 to i32 because no loss of bit information
+        if (self.0 as i32) == *other {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+impl PartialEq<i32> for Index<u32> {
+    fn eq(&self, other: &i32) -> bool {
+        // When the i32 value 'other' is negative, it cannot equal a u32.
+        // Handle this condition first.
+        if *other < 0 {
+            return false;
+        }
+
+        // When the i32 value 'other' is positive, or zero, we can safely 'upcast' it to a u32.
+        if self.0 == (*other as u32) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+pub(crate) struct KeyVector<T: Sized, I: UnsignedType, const N: usize> {
     length: usize,
     capacity: usize,
-    indices: [I; N],
+    indices: [Index<I>; N],
     data: [T; N],
 }
 
@@ -44,17 +94,15 @@ impl WebCore {
         WebCore { wasm_allocator }
     }
 
-    pub(super) fn init(&mut self) {
-        //        let returned_ptr = unsafe { self.wasm_allocator.internal_alloc(1) };
-        //        console_log!("Returned ptr: {:?}", returned_ptr);
-    }
-
     // Until Rust permits 'Placement New' logic, we have to initialize the KeyVector by directly
     // writing bytes into the backing pool.
     // Once Placement New is possible, we can ideally separate the KeyVector back into its own
     // module (we currently need it placed here to access the private fields - so we can write
     // bytes to these private fields).
-    pub(super) fn addkeyvec<T: Sized, I: IndexVariable, const N: usize>(&mut self) {
+    pub(super) fn addkeyvec<T: Sized, I: UnsignedType, const N: usize>(&mut self)
+    where
+        Index<I>: PartialEq<i32>,
+    {
         let tracking_ptr_usize = self.wasm_allocator.tracking_ptr as usize;
 
         // This check fills the role of a runtime assert that N != 0 which ideally would be placed
@@ -82,6 +130,19 @@ impl WebCore {
             panic!();
         }
 
+        if self.wasm_allocator.allocation_size < size_of::<KeyVector<T, I, N>>() {
+            console_log!("[WebCore::addkeyvec()] ERROR: Allocation not large enough!");
+            console_log!(
+                "[WebCore::addkeyvec()] Allocation Size: {}",
+                self.wasm_allocator.allocation_size
+            );
+            console_log!(
+                "[WebCore::addkeyvec()] KeyVector Size: {}",
+                size_of::<KeyVector<T, I, N>>()
+            );
+            panic!();
+        }
+
         // At this stage:
         // tracking_ptr is suitably aligned to be converted into *mut KeyVector<T, I, N>
         let tracking_ptr = self.wasm_allocator.tracking_ptr as *mut KeyVector<T, I, N>;
@@ -92,6 +153,8 @@ impl WebCore {
         unsafe {
             addr_of_mut!((*tracking_ptr).length).write_bytes(0, 1);
             addr_of_mut!((*tracking_ptr).capacity).write_bytes(0, 1);
+
+            // WE *MUST* CONFIRM THIS ZEROS THE ENTIRE ARRAY!!!
             addr_of_mut!((*tracking_ptr).indices).write_bytes(0, 1);
 
             assert!(
@@ -99,11 +162,21 @@ impl WebCore {
                 "Array sizing check"
             );
 
+            // This confirms that all values within the array [Index<I>; N] are properly ZEROED.
+            // This is ONLY FOR TESTING PURPOSES, ensures that the code above:
+            //
+            // addr_of_mut!((*tracking_ptr).indices).write_bytes(0, 1);
+            //
+            // ... has indeed cleared the entire array.
             for i in 0..(*tracking_ptr).indices.len() {
-                console_log!("I: {:?}", i);
-                //                if (*tracking_ptr).indices[i] != 0 {
-                //                    panic!();
-                //                }
+                if (*tracking_ptr).indices[i] != 0 {
+                    console_log!(
+                        "Invalid zeroing!!! I: {:?}, Value: {:?}",
+                        i,
+                        (*tracking_ptr).indices[i].0
+                    );
+                    panic!();
+                }
             }
         }
     }
